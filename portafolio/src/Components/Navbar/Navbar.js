44e817d18d05429e02287d5773d1fc7b5995032f
Cristofer Venegas Villalobos <108409614+CrisVillavega@users.noreply.github.com>
2024-11-30 20:36:53 -0600

    vertical navbar style

--- portafolio/src/Components/Navbar/Navbar.js
+++ portafolio/src/Components/Navbar/Navbar.js
@@ -1,46 +1,52 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faFacebook, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 function Navbar() {
   const [searchVisible, setSearchVisible] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const showSearch = () => {
-    setSearchVisible(true);
+  const toggleSearch = () => {
+    setSearchVisible(!searchVisible);
   };
 
-  const hideSearch = () => {
-    setSearchVisible(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <nav className="navbar" >
-      <div className="navbar-social">
-        <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
-        <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
-        <a href="#"><FontAwesomeIcon icon={faWhatsapp} /></a>
-      </div>
+    <nav className="navbar">
+      <button className="navbar-burger" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+      </button>
       <div className="navbar-logo">
         <h1>Cristofer Villavega</h1>
       </div>
-      <div className="navbar-links">
-        <a href="#">HOME</a>
-        <a href="#">DASBOARD</a>
-        <a href="#">PROYECT</a>
-        <a href="#">GALLERY</a>
-        <a href="#">GAMES</a>
+      <div className={`navbar-menu ${menuOpen ? 'active' : ''}`}>
+        <div className="navbar-social">
+          <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+          <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+          <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
+          <a href="#"><FontAwesomeIcon icon={faWhatsapp} /></a>
+        </div>
+        <div className="navbar-links">
+          <a href="#">HOME</a>
+          <a href="#">DASBOARD</a>
+          <a href="#">PROYECT</a>
+          <a href="#">GALLERY</a>
+          <a href="#">GAMES</a>
+        </div>
       </div>
       <div className="navbar-actions">
         {searchVisible ? (
           <div className="search-container">
             <input type="text" placeholder="Search..." />
-            <button onClick={hideSearch}><FontAwesomeIcon icon={faTimes} /></button>
+            <button onClick={toggleSearch}><FontAwesomeIcon icon={faTimes} /></button>
           </div>
         ) : (
-          <button onClick={showSearch}><FontAwesomeIcon icon={faSearch} /></button>
+          <button onClick={toggleSearch}><FontAwesomeIcon icon={faSearch} /></button>
         )}
       </div>
     </nav>
